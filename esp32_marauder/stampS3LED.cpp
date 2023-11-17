018253b07894bcf45a08b22a9bcf4eb09c78b352
João Belo <joaobelo.dev@gmail.com>
2023-11-17 22:25:31 +0000

    commnet fastLED

--- esp32_marauder/stampS3LED.cpp
+++ esp32_marauder/stampS3LED.cpp
@@ -1,19 +1,19 @@
 #include "stampS3LED.h"
-#include <FastLED.h>
+// #include <FastLED.h>
 
 void stampS3LED::RunSetup() {
-  FastLED.addLeds<WS2812, STAMP_S3_LED_PIN, GRB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812, STAMP_S3_LED_PIN, GRB>(leds, NUM_LEDS);
 
-  leds[0] = CHSV(0, 255, 255);
-  FastLED.show();
-  delay(500);
+  // leds[0] = CHSV(0, 255, 255);
+  // FastLED.show();
+  // delay(500);
 
-  leds[0] = CHSV(0, 0, 255);
-  FastLED.show();
-  delay(500);
+  // leds[0] = CHSV(0, 0, 255);
+  // FastLED.show();
+  // delay(500);
 
-  leds[0] = CHSV(255, 0, 255);
-  FastLED.show();
+  // leds[0] = CHSV(255, 0, 255);
+  // FastLED.show();
 
   // pinMode(B_PIN, OUTPUT);
   // pinMode(G_PIN, OUTPUT);
